715dc8c278231dd5013a471be93ca423f9acd1db
reyhanmahardhika <reyhanmahardhika24@gmail.com>
2020-07-12 16:03:35 +0700

    terbaru

--- app/src/main/java/com/example/myfristaop/peluangusaha/MapsActivity.kt
+++ app/src/main/java/com/example/myfristaop/peluangusaha/MapsActivity.kt
@@ -1,20 +1,21 @@
 package com.example.myfristaop.peluangusaha
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Address
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Camera
+import android.location.Geocoder
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.*
-import android.location.Geocoder
+import kotlinx.android.synthetic.main.activity_maps.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -22,13 +23,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     var kelurahan=""
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val toolbar = findViewById <Toolbar> (R.id.app_toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.navigationIcon = ContextCompat.getDrawable(this,R.drawable.ic_action_name)
+        getSupportActionBar()!!.setDisplayShowTitleEnabled(false)
+
+        toolbar.setNavigationOnClickListener { Toast.makeText(applicationContext,"Navigation icon was clicked",Toast.LENGTH_SHORT).show() }
     }
 
     /**
@@ -45,35 +57,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         var markerAwal=0
         val medan = LatLng( 3.597031, 98.678513)
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(medan,13F))
+        val cu = CameraUpdateFactory.newLatLngZoom(medan,13F)
+        mMap.animateCamera(cu)
 
         mMap.setOnMapClickListener(GoogleMap.OnMapClickListener { LatLng ->
             val position = LatLng
 
             if(markerAwal==0){
                 addMarker(position)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position,15.7F))
                 markerAwal=1
             }
             else {
                 mMap.clear()
                 addMarker(position)
+                val zoom =mMap.cameraPosition.zoom.toDouble()
+                if(zoom<15.7) {
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position,15.7F))
+                }
+                else{
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(position))
+                }
             }
         })
     }
-    private fun addMarker(latLng: LatLng){
-        val marker= mMap.addMarker(MarkerOptions()
+    private fun addMarker(latLng: LatLng) {
+        val marker = mMap.addMarker(MarkerOptions()
                 .position(latLng).draggable(true).title(getAddress(latLng)))
         marker.showInfoWindow()
+        marker.isDraggable=false
         val circle: Circle = mMap.addCircle(CircleOptions().center(latLng).radius(500.0))
-
     }
-
     private fun getAddress(latLng: LatLng): String {
         val geocoder = Geocoder(this)
         val list = geocoder.getFromLocation(latLng.latitude,latLng.longitude, 1)
-        val tmpKelurahan= list[0].subLocality
-        return tmpKelurahan
+        kelurahan= list[0].subLocality
+
+        txt_cari.setText(list[0].getAddressLine(0).toString())
+        return kelurahan
     }
+
 
 
 }
