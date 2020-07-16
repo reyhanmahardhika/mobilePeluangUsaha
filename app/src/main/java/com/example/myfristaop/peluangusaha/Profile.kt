d0c08d83e297e7e0988bfb292260e2b4e95cfdf8
wendisaputra <wendisaputra992@gmail.com>
2020-07-16 19:29:02 +0800

    Profile

---
+++ app/src/main/java/com/example/myfristaop/peluangusaha/Profile.kt
@@ -0,0 +1,15 @@
+package com.example.myfristaop.peluangusaha
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Profile : Fragment(){
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val view = inflater.inflate(R.layout.activity_profile, container, false)
+    return view
+  }
+}
